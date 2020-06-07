3524ce586c2b193d08eb6bd0bea4881764cbfac0
Elie Hymowitz <ehymowitz@gmail.com>
2020-06-07 13:52:45 -0400

    renamed layout to top nav

--- src/components/topnav.js
+++ src/components/topnav.js
@@ -3,15 +3,15 @@ import { Link } from "gatsby"
 import { css } from "@emotion/core"
 import linkStyles from "../styles/components/layout.module.scss"
 
-export default function Layout({ children }) {
+export default function TopNav({ children }) {
   return (
-    <div
-      css = {css`
+    <div>
+      <ul
+        css={css`
         margin: 3rem auto;
         maxWidth: 650;
       `}
-    >
-      <ul>
+      >
         <Link className= {linkStyles.link} to="/projects">Projects</Link>
         <Link className= {linkStyles.link} to="/music/">Music</Link>
         <Link className= {linkStyles.link} to="/photography/">Photography</Link>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
 import React from "react"
-import Layout from "../components/layout"
+import TopNav from "../components/topnav"
 
 
 export default function Home() {
   return (
-    <Layout>
+    <TopNav>
 	    <h1>Hi, I'm Elie</h1>
 	    <h2>I like making things</h2>
-    </Layout>
+    </TopNav>
   )
 }
