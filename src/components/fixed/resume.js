59124e0320fc897b0ec5db432b11022157465359
Elie Hymowitz <ehymowitz@gmail.com>
2020-06-09 19:44:24 -0400

    updated resume

--- src/components/fixed/resume.js
+++ src/components/fixed/resume.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Resume() {
   return (
     <div className="resume">
-        <a href="../../ElieHymowitz.pdf" target="_blank">CV</a>
+        <a href="../../elie-hymowitz.pdf" target="_blank">CV</a>
     </div>
   )
 }
