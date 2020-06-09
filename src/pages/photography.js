25a8dfcf1d29bab1454367b66720bb708ed7b2b0
Elie Hymowitz <ehymowitz@gmail.com>
2020-06-09 13:04:44 -0400

    photos start

--- src/pages/photography.js
+++ src/pages/photography.js
@@ -1,10 +1,14 @@
 import React from "react"
+import Collection from "../components/cards/collection"
 
 export default function Photography() {
   return (
     <div className='page-section' id='photography'>
       <div className="heading">
         <h1>Photos I've Taken</h1>
+      </div>
+      <div className="photo-collections">
+        <Collection></Collection>
       </div>
     </div>
   )
