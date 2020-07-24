01b8eecfde8ce3b7050df0077fe5d14eaa18cde5
Elie Hymowitz <ehymowitz@gmail.com>
2020-07-24 15:17:36 -0400

    Added SEO tags

--- src/components/fixed/layout.js
+++ src/components/fixed/layout.js
@@ -6,7 +6,12 @@ export default function Layout({ children }) {
     <div className="layout">
       <Helmet defer={false} >
         <title>Elie Hymowitz</title>
-        <meta name="description" content= "Elie Hymowitz's Web Portfolio. I'm a web developer and musician, and I take photos too."/>
+        <meta name="description" content= "Elie Hymowitz's Web Portfolio. I'm a web developer and musician, and I take photos too." />
+        <meta property="og:title" content='Elie Hymowitz' />
+        <meta property="og:description" content="Elie Hymowitz's Web Portfolio. I'm a web developer and musician, and I take photos too." />
+        <meta property="og:image" content='/images/headshot.jpeg' />
+        <meta property="og:url" content='https://hymowitz.org/'/>
+        <meta property="og:type" content='website'/>
       </Helmet>
       {children}
     </div>
