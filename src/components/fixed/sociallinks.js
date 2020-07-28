43a834370dc412aa2fbc04cd9bc31ada612b73a5
Elie Hymowitz <ehymowitz@gmail.com>
2020-07-28 10:36:01 -0400

    init

--- src/components/fixed/sociallinks.js
+++ src/components/fixed/sociallinks.js
@@ -1,10 +1,40 @@
 import React from "react"
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
 import {
   FaFacebookF, FaInstagram, FaSpotify, FaLinkedinIn, FaGithub
 } from 'react-icons/fa';
 import { AiOutlineMail } from "react-icons/ai"
 
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
 export default function SocialLinks() {
+
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <div className='social-links'>
       <ul>
