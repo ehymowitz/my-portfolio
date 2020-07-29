ea5a19ab22cb36b8c2371a1ac4f04a789130ba2c
Elie Hymowitz <ehymowitz@gmail.com>
2020-07-29 00:29:39 -0400

    fixed link warning

--- src/components/cards/photo-cover.js
+++ src/components/cards/photo-cover.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby"
 export default function PhotoCover({ link, cover, title, camera }) {
   const photolink = "./collections/" + link
   return(
-    <Link className="collection-cover" to={photolink}>
+    <Link className="collection-cover" to={`/${photolink}`}>
       <img src={cover} alt={title}/>
       <div className="collection-text">
         <h3>{title}</h3>
