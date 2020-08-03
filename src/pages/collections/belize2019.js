6f39518167e3c7feb1bb43087ea3755a3d155f59
Elie Hymowitz <ehymowitz@gmail.com>
2020-08-03 16:47:04 -0400

    added model but picture wont change

--- src/pages/collections/belize2019.js
+++ src/pages/collections/belize2019.js
@@ -5,18 +5,61 @@ import Fixed from "../../components/fixed/fixed"
 import { graphql } from "gatsby"
 import Img from "gatsby-image"
 import Masonry from 'react-masonry-css'
+import { makeStyles } from '@material-ui/core/styles'
+import Modal from '@material-ui/core/Modal'
+import Backdrop from '@material-ui/core/Backdrop'
+import Fade from '@material-ui/core/Fade'
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: "#F5F5F5",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+    borderRadius: "10px",
+    outline: 'none',
+  },
+}));
+
 
 export default function PhotoCollection({ data }) {
-  const photos = data.allFile.nodes.map( p => p.childImageSharp.fluid)
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  var picture = data.allFile.nodes[1].childImageSharp.fluid
+
+  const handleOpen = (event) => {
+    let target = event.target.parentElement.parentElement.parentElement.dataset.id
+    picture = data.allFile.nodes[target].childImageSharp.fluid
+    console.log(picture)
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const photos = data.allFile.nodes.map( p => p)
 
   const items = photos.map((item, i) => {
-    return <Img
-      fluid={item}
-      key={i}
-      style={{
-        margin: "0px 10px 10px 0px"
-      }}
-    />
+    return (
+      <div onClick={handleOpen}
+           onKeyPress={handleOpen}
+           role="button"
+           tabIndex="-1"
+           key={i}
+           className="picture-modal"
+           data-id={i}
+      >
+        <Img
+          fluid={item.childImageSharp.fluid}
+        />
+      </div>
+    )
   });
 
   // TODO: Impliment modal
@@ -39,6 +82,24 @@ export default function PhotoCollection({ data }) {
           >
           {items}
         </Masonry>
+        <Modal
+          aria-labelledby="transition-modal-title"
+          aria-describedby="transition-modal-description"
+          className={classes.modal}
+          open={open}
+          onClose={handleClose}
+          closeAfterTransition
+          BackdropComponent={Backdrop}
+          BackdropProps={{
+            timeout: 500,
+          }}
+        >
+          <Fade in={open}>
+            <div>
+              <Img fluid={picture} style={{width: "50vw" }}/>
+            </div>
+          </Fade>
+        </Modal>
         <Photography
           heading="Other Collections">
         </Photography>
@@ -51,6 +112,7 @@ export const query = graphql`
   query {
     allFile(filter: {relativePath: {regex: "/photos\\/collections\\/belize/"}}, sort: {fields: relativePath, order: ASC}) {
       nodes {
+        id
         childImageSharp {
           fluid {
             ...GatsbyImageSharpFluid
