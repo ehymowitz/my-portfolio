df950dc15eaa2164d0e28d18877ca72c1128dad3
Elie Hymowitz <ehymowitz@gmail.com>
2020-08-30 13:37:45 -0400

    added portfolio to projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -22,27 +22,37 @@ export default function Projects() {
           desc="Mezcal Montreal landing page"
           stack={["Gatsby.png", "MaterialUI.svg", "Sass.png", "GraphQL.svg", "Getform.svg"]}
           api="GetForm connected to Zapier and Google Docs for event registration"
-          image={photos.allFile.edges[2].node.childImageSharp.fluid}
+          image={photos.mezcal.edges[0].node.childImageSharp.fluid}
           >
         </Project>
         <Project
           orient="left"
+          link="http://hymowitz.org/"
+          title="This very website"
+          desc="My portfolio! Where I get to show off my superpowers"
+          stack={["Gatsby.png", "GraphQL.svg", "Sass.png", "Netlify.png"]}
+          api=""
+          image={photos.portfolio.edges[0].node.childImageSharp.fluid}
+          >
+        </Project>
+        <Project
+          orient="right"
           link="http://www.dontdiefood.com/"
           title="Don't Die Food"
           desc="Mobile-first webapp for sharing food in local communities"
           stack={["Rails.svg", "Ruby.svg", "HTML5.svg", "CSS3.png", "Javascript.png"]}
           api="Spoonacular for recipe generation"
-          image={photos.allFile.edges[0].node.childImageSharp.fluid}
+          image={photos.DDF.edges[0].node.childImageSharp.fluid}
           >
         </Project>
         <Project
-          orient="right"
+          orient="left"
           link="https://blockwagon.herokuapp.com/"
           title="Blockwagon"
           desc="Online marketplace for VHS tapes"
           stack={["Rails.svg", "Ruby.svg", "HTML5.svg", "CSS3.png", "Javascript.png"]}
           api="OMDB for generating video information"
-          image={photos.allFile.edges[1].node.childImageSharp.fluid}
+          image={photos.blockwagon.edges[0].node.childImageSharp.fluid}
           >
         </Project>
       </div>
@@ -52,7 +62,43 @@ export default function Projects() {
 
 const query = graphql`
   query projectsQuery {
-    allFile(filter: {absolutePath: {regex: "/projects/"}}) {
+    mezcal: allFile(filter: {absolutePath: {regex: "/MezcalMTL/"}}) {
+      edges {
+        node {
+          id
+          childImageSharp {
+            fluid(fit: COVER) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+    portfolio: allFile(filter: {absolutePath: {regex: "/Portfolio/"}}) {
+      edges {
+        node {
+          id
+          childImageSharp {
+            fluid(fit: COVER) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+    DDF: allFile(filter: {absolutePath: {regex: "/DDF/"}}) {
+      edges {
+        node {
+          id
+          childImageSharp {
+            fluid(fit: COVER) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+    blockwagon: allFile(filter: {absolutePath: {regex: "/Blockwagon/"}}) {
       edges {
         node {
           id
